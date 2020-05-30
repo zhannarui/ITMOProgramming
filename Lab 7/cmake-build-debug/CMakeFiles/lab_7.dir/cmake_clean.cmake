146ca61ceb871fe93afdd2f60bb91db38fcca1f9
Zhanna <58556862+zhannarui@users.noreply.github.com>
2020-05-30 10:40:11 +0300

    Add files via upload

---
+++ Lab 7/cmake-build-debug/CMakeFiles/lab_7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_7.dir/main.cpp.o"
+  "lab_7"
+  "lab_7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab 7/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "CircleBuffer.h"
+
+int main() {
+    Circle<int> CircleBuffer(5);
+    Circle<int>::Iterator it = CircleBuffer.Begin();
+    for (int i = 1; i <= 10; i++) {
+        CircleBuffer.PushFront(i);
+    }
+    std::cout << "Выведем состояние буфера при помощи итератора: ";
+    for (it = CircleBuffer.Begin(); it != CircleBuffer.End(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << *CircleBuffer.End() << std::endl;
+    std::cout << "Удалим первый элемент:\n";
+    CircleBuffer.DeleteFirst();
+    std::cout << "Состояние буфера: ";
+    for (int i = 0; i < CircleBuffer.Size(); i++)
+        std::cout << CircleBuffer[i] << " ";
+
+    std::cout << "\nУдалим последний элемент:\n";
+    CircleBuffer.DeleteLast();
+    std::cout << "Состояние буфера: ";
+    for (int i = 0; i < CircleBuffer.Size(); i++)
+        std::cout << CircleBuffer[i] << " ";
+
+    std::cout << "Удаляем оставшиеся элементы при помощи интератора:\n";
+    it = CircleBuffer.Begin();
+    while (!CircleBuffer.isEmpty()) {
+        CircleBuffer.Delete(it);
+        it++;
+    }
+    std::cout << "Состояние буфер: ";
+    for (int i = 0; i < CircleBuffer.Size(); i++)
+        std::cout << CircleBuffer[i] << " ";
+    std::cout << "\nУвеличиваем вместимость буфера до 10\n";
+    CircleBuffer.Resize(10);
+
+
+    std::cout << "Вставляем новые числа: \n";
+    for (int i = 1; i <= 10; i++) {
+        CircleBuffer.PushBack(i);
+    }
+    std::cout << "Состояние буфер: ";
+    for (int i = 0; i < CircleBuffer.Size(); i++)
+        std::cout << CircleBuffer[i] << " ";
+    std::cout << std::endl;
+
+
+    std::cout << "Заменим Первый элемент, последний и 8-ой:\n";
+    *(CircleBuffer.Begin()) = -7;
+    *(CircleBuffer.End()) = -9;
+    CircleBuffer[7] = -5;
+    std::cout << "Состояние буфера: ";
+    for (int i = 0; i < CircleBuffer.Size(); i++)
+        std::cout << CircleBuffer[i] << " ";
+    std::cout << std::endl;
+
+
+}
